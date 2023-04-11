8d8ac088de33823c769a70f2f86fbd7605869417
Jonathan Nunes <devjonathannunes@gmail.com>
2023-04-11 12:59:48 -0300

    update api

--- src/apis/Api.ts
+++ src/apis/Api.ts
@@ -1,6 +1,5 @@
 import axios from 'axios';
 import { CarouselItemType, CastType, MovieType, SerieType } from '../types/SearchType';
-import { log } from 'console';
 
 const http = axios.create({
     baseURL: 'https://api.themoviedb.org/3/'
