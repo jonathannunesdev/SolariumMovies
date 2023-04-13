448cf6fbfe7e760ecdfdc63170be5de4befd6b97
Jonathan Nunes <devjonathannunes@gmail.com>
2023-04-13 10:39:51 -0300

    update routes

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -2,18 +2,24 @@ import { useRoutes } from "react-router-dom";
 
 import { SearchList } from "../pages/searchList";
 import { Home } from "../pages/home";
+import { Login } from "../pages/login";
 import { Logged } from "../pages/logged";
 import { DetailedSearch } from "../pages/detailedSearch";
 import { NoResults } from "../pages/noResults";
 import { Favorites } from "../pages/favorites";
+import { ResetPassword } from "../pages/forgotPassword";
+import { PrivacyPolicy } from "../pages/privacyPolicy";
 
 export const Routes = () => {
   return useRoutes([
     { path: "/", element: <Home /> },
     { path: "/favorites", element: <Favorites /> },
+    { path: '/login', element: <Login />},
     { path: "/logged", element: <Logged /> },
     { path: "/searchlist", element: <SearchList /> },
     { path: "/noResults", element: <NoResults /> },
+    { path: "/resetPassword", element: <ResetPassword /> },    
+    { path: "/privacy-policy", element: <PrivacyPolicy/>},
     { path: "/detailed/:media_type/:id", element: <DetailedSearch /> },
   ]);
 };
