a9afbcbb6e62d317c8369a02a466143fbfea2b7a
Jonathan Nunes <devjonathannunes@gmail.com>
2023-04-13 10:34:07 -0300

    update privacy policy

---
+++ src/pages/privacyPolicy/index.tsx
@@ -0,0 +1,143 @@
+import { Container } from "./styles";
+
+export const PrivacyPolicy = () => {
+  return (
+    <Container>
+      <div>
+        <h1>Política de Privacidade - Solarium Movies</h1>
+        <p>
+          Esta Política de Privacidade descreve como as informações pessoais são
+          coletadas, usadas e compartilhadas quando você visita ou faz uma
+          compra em https://solariummovies.netlify.app/ (o “Site”).
+        </p>
+        <h2>1. Informações Pessoais que Coletamos</h2>
+        <p>
+          Quando você visita o Site, automaticamente coletamos determinadas
+          informações sobre o seu dispositivo, incluindo informações sobre o seu
+          navegador da web, endereço IP, fuso horário e alguns dos cookies que
+          estão instalados em seu dispositivo. Além disso, à medida que você
+          navega no Site, coletamos informações sobre as páginas da web ou
+          produtos individuais que você visualiza, quais sites ou termos de
+          pesquisa indicaram você ao Site e informações sobre como você interage
+          com o Site. Referimo-nos a essas informações coletadas automaticamente
+          como “Informações do dispositivo”.
+        </p>
+        <h2>2. Coletamos informações usando as seguintes tecnologias:</h2>
+        <ul>
+          <li>
+            “Cookies” são arquivos de dados que são colocados no seu dispositivo
+            ou computador e geralmente incluem um identificador exclusivo
+            anônimo. Para mais informações sobre cookies e como desativá-los,
+            visite http://www.allaboutcookies.org.
+          </li>
+          <li>
+            “Log files” rastreiam as ações ocorridas no Site e coletam dados,
+            incluindo seu endereço IP, tipo de navegador, provedor de serviços
+            de Internet, páginas de referência / saída e carimbos de data /
+            hora.
+          </li>
+          <li>
+            “Web beacons”, “tags” e “pixels” são imagens eletrônicas usadas para
+            registrar informações sobre como você navega no Site.
+          </li>
+        </ul>
+        <h2>3. Compartilhando suas Informações Pessoais</h2>
+        <p>
+          Compartilhamos suas Informações Pessoais com terceiros para nos ajudar
+          a usar suas Informações Pessoais, conforme descrito acima. Por
+          exemplo, usamos o Google Analytics para nos ajudar a entender como
+          nossos clientes usam o Site - você pode ler mais sobre como o Google
+          usa suas Informações Pessoais aqui:
+          https://www.google.com/intl/pt-BR/policies/privacy/. Você também pode
+          optar por não participar do Google Analytics aqui:
+          https://tools.google.com/dlpage/gaoptout.
+        </p>
+        <h2>4. Publicidade Comportamental</h2>
+        <p>
+          Como descrito acima, usamos suas Informações Pessoais para fornecer
+          anúncios direcionados ou comunicações de marketing que acreditamos
+          serem do seu interesse. Para obter mais informações sobre como a
+          publicidade direcionada funciona, você pode visitar a página
+          educacional da Network Advertising Initiative (“NAI”) em
+          http://www.networkadvertising.org/understanding-online-advertising/how-does-it-work.
+        </p>
+        <h2>5. Segurança</h2>
+        <p>
+          Nós nos esforçamos para proteger suas informações pessoais, empregando
+          medidas de segurança razoáveis para proteger suas informações contra
+          acesso, uso ou divulgação não autorizados.
+        </p>
+        <h2>6. Links para sites de terceiros</h2>
+        <p>
+          Nosso Site pode conter links para sites de terceiros. Não somos
+          responsáveis pelas práticas de privacidade desses sites. Esta Política
+          de Privacidade se aplica apenas às informações coletadas pelo nosso
+          Site.
+        </p>
+        <h2>7. Alterações na Política de Privacidade</h2>
+        <p>
+          Reservamo-nos o direito de modificar esta Política de Privacidade a
+          qualquer momento. As alterações entrarão em vigor imediatamente após a
+          publicação da Política de Privacidade atualizada no Site. Sua
+          continuidade no uso do Site após tais alterações constitui sua
+          aceitação da Política de Privacidade atualizada.
+        </p>
+        <h2>8. Contate-nos</h2>
+        <p>
+          Se você tiver alguma dúvida ou preocupação sobre esta Política de
+          Privacidade ou nossas práticas de privacidade, entre em contato
+          conosco através do e-mail jonathan.ribeiro61@gmail.com.
+        </p>
+        <h2>9. Uso de cookies e outras tecnologias de rastreamento</h2>
+        <p>
+          Uso de cookies e outras tecnologias de rastreamento Podemos usar
+          cookies e outras tecnologias de rastreamento para coletar informações
+          sobre o uso do Site e melhorar sua experiência. Ao utilizar nosso
+          Site, você concorda com o uso de cookies e outras tecnologias de
+          rastreamento, de acordo com esta Política de Privacidade.
+        </p>
+        <h2>10. Transferências internacionais de dados</h2>
+        <p>
+          Seus dados podem ser transferidos e armazenados em servidores
+          localizados fora do seu país de residência, que podem ter leis de
+          proteção de dados diferentes das do seu país. Ao usar nosso Site, você
+          concorda com a transferência de suas informações para esses
+          servidores.
+        </p>
+        <h2>11. Direitos de acesso, retificação e exclusão de dados</h2>
+        <p>
+          Você tem o direito de acessar, retificar e excluir suas informações
+          pessoais, conforme permitido por lei. Para exercer esses direitos,
+          entre em contato conosco através do e-mail jonathan.ribeiro61@gmail.com.
+        </p>
+        <h2>12. Retenção de dadosl</h2>
+        <p>
+          Retemos suas informações pessoais pelo tempo necessário para cumprir
+          os propósitos descritos nesta Política de Privacidade, a menos que
+          exigido de outra forma por lei.
+        </p>
+        <h2>13. Menores de idade</h2>
+        <p>
+          Nosso Site não é destinado a menores de idade. Não coletamos
+          intencionalmente informações pessoais de menores de idade. Se você é
+          menor de idade, por favor, não use ou forneça informações pessoais
+          através do Site.
+        </p>
+        <h2>14. Alterações a esta Política de Privacidade</h2>
+        <p>
+          Reservamo-nos o direito de modificar esta Política de Privacidade a
+          qualquer momento. As alterações entrarão em vigor imediatamente após a
+          publicação da Política de Privacidade atualizada no Site. Sua
+          continuidade no uso do Site após tais alterações constitui sua
+          aceitação da Política de Privacidade atualizada.
+        </p>
+        <h2>15. Contate-nos</h2>
+        <p>
+          Se você tiver alguma dúvida ou preocupação sobre esta Política de
+          Privacidade ou nossas práticas de privacidade, entre em contato
+          conosco através do e-mail jonathan.ribeiro61@gmail.com.
+        </p>
+      </div>
+    </Container>
+  );
+};
---
+++ src/pages/privacyPolicy/styles.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    min-height: 100vh;
+    max-width: 1000px;
+    margin: auto;
+    margin-top: 10vh;
+    padding: 50px 0;
+    color: #888;
+`;
